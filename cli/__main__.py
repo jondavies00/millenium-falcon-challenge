3c20d27663b8f5aa23dfd93388ecd672fec51b9f
Jonathan Davies <jonathandavies128@outlook.com>
2024-04-27 00:20:10 +0100

    README, CLI, and QoL changes

--- cli/__main__.py
+++ cli/__main__.py
@@ -1,3 +1,43 @@
 """
 Uses argparse to load in JSONs and calculate a percentage score 
 """
+import sys
+
+from falcon_solver.solver.solver import Solver
+print(sys.path)
+import argparse
+import fnmatch
+import json
+from pathlib import Path
+import re
+
+from core.falcon_solver.shared.models import EmpireConfiguration, FalconConfiguration
+from core.falcon_solver.parser.parse_json import parse_json
+
+# def parse_data()
+
+if __name__ == "__main__":
+    odds_parser = argparse.ArgumentParser(prog="falcon_solver", description="Calculate the odds for given millennium falcon data & empire plans in JSON format.")
+
+    odds_parser.add_argument('-m', '--millennium-falcon-data', dest='millennium_falcon_data', type=str, default=None, help='The millennium falcon data in JSON format.')
+    odds_parser.add_argument('-e', '--empire-data', dest='empire_data', type=str, default=None, help='The empire plans data in JSON format.')
+
+    args, unknown = odds_parser.parse_known_args()
+
+    parser = argparse.ArgumentParser(parents=[odds_parser], add_help=False)
+
+    if args.millennium_falcon_data is None or args.empire_data is None:
+        raise argparse.ArgumentTypeError("Args must contain valid millennium falcon and empire JSON files.")
+    falcon_data_fn = args.millennium_falcon_data
+    empire_data_fn = args.empire_data
+    
+    if fnmatch.fnmatch(falcon_data_fn, "*.json") and fnmatch.fnmatch(empire_data_fn, "*.json"):
+        print("valid json")
+        falcon_config = parse_json(falcon_data_fn, FalconConfiguration)
+        empire_config = parse_json(empire_data_fn, EmpireConfiguration)
+        # falcon_config = FalconConfiguration(**json.load(open(data_fn)))
+        print(Solver(falcon_config, empire_config).tell_me_the_odds())
+        print(falcon_config)
+        print(empire_config)
+    else:
+        argparse.ArgumentTypeError(f"One of the data files were not valid JSON.")
--- core/falcon_solver/parser/parse_universe.py
+++ core/falcon_solver/parser/parse_universe.py
@@ -4,8 +4,12 @@ import sqlite3
 
 
 def parse_universe(db_path: str) -> dict[dict[str, int]]:
+    full_path = str(Path.cwd()) + db_path
+    if "core" not in full_path:
+        full_path = str(Path.cwd()) + "/core" + db_path
+        print(full_path)
     logging.info(str(Path.cwd()) + db_path)
-    connection = sqlite3.connect(str(Path.cwd()) + db_path)
+    connection = sqlite3.connect(full_path)
     cur = connection.cursor()
     res = cur.execute("SELECT * FROM routes;")
     results = res.fetchall()
